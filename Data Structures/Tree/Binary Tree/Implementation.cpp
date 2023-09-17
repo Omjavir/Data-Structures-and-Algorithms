5231370673d4e076e2e15eb9b41d6ba0c6147d7c
Om Suresh Javir <javirom07042003@gmail.com>
2023-09-17 11:06:02 +0530

    Implemented Binary Tree

--- Data Structures/Tree/Binary Tree/Implementation.cpp
+++ Data Structures/Tree/Binary Tree/Implementation.cpp
@@ -1,7 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+    }
+};
+
+void levelOrderTraversal(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+
+Node *buildTree(Node *root)
+{
+    cout << "Enter the data : " << endl;
+    int data;
+    cin >> data;
+    root = new Node(data);
+
+    if (data == -1)
+    {
+        return NULL;
+    }
+
+    cout << "Enter the data for inserting in left side of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter the data for inserting in right side of " << data << endl;
+    root->right = buildTree(root->right);
+
+    return root;
+}
+
 int main()
 {
+    Node *root = NULL;
+
+    root = buildTree(root);
+    cout << "Level Order Traversal : " << endl;
+    levelOrderTraversal(root);
+    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
 
     return 0;
 }
