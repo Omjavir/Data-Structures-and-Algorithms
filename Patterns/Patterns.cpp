42869af5e7cbbb380bbef6e2281d1830b2709c4e
Omjavir <javirom07042003@gmail.com>
2024-01-27 13:34:49 +0530

    patterns

---
+++ Patterns/Patterns.cpp
@@ -0,0 +1,194 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+// Pattern 1
+/*
+	* * *
+	* * *
+	* * *
+*/
+for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cout << "* ";
+		}
+		cout << endl;
+	}
+
+// Pattern 2
+/*
+	*
+	* *
+	* * *
+*/
+for(int i = 0; i <= n; i++){
+		for(int j = 0; j < i; j++){
+			cout << "* ";
+		}
+		cout << endl;
+	}
+
+	// Pattern 3
+	/*
+		1
+		1 2
+		1 2 3
+	*/
+for(int i = 1; i <= n; i++){
+  		for(int j = 1; j <= i; j++){
+  			cout << j << " ";
+  		}
+  		cout << endl;
+  	}
+    
+// Pattern 4
+/*
+	1
+	2 2
+	3 3 3
+*/
+for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= i; j++){
+			cout << i << " ";
+		}
+		cout << endl;
+	}
+
+// Pattern 5
+/*
+	* * * * * 
+	* * * * 
+	* * * 
+	* * 
+	* 
+*/
+for(int i = 0; i < n; i++){
+      for(int j = 0; j < n - i; j++){
+        cout << "* ";
+      }
+      cout << endl;
+    }
+
+// Pattern 6
+/*
+	1 2 3 4 5 
+	1 2 3 4 
+	1 2 3 
+	1 2 
+	1 
+*/
+for(int i = 1; i <= n; i++){
+      for(int j = 1; j <= n - i + 1; j++){
+        cout << j << " ";
+      }
+      cout << endl;
+    }
+
+// Pattern 7
+/*
+	*    
+   ***   
+  *****  
+ ******* 
+*********
+*/
+for(int i = 0; i < n; i++){
+        // space 
+        for(int j = 0; j < n - i - 1; j++){
+          cout << " ";
+        }
+        
+        // star
+        for(int j = 0; j < 2 * i + 1; j++){
+          cout << "*";
+        }
+        
+        // space
+        for(int j = 0; j < n - i - 1; j++){
+          cout << " ";
+        }
+        
+        cout << endl;
+    }
+
+// pattern 8
+/*
+*********
+ ******* 
+  *****  
+   ***   
+    *   
+*/
+for(int i = 0; i < n; i++){
+        // space 
+        for(int j = 0; j < i; j++){
+          cout << " ";
+        }
+        
+        // star
+        for(int j = 0; j < 2 * n - (2 * i + 1); j++){
+            cout << "*";
+        }
+        
+        // space
+        for(int j = 0; j < i; j++){
+          cout << " ";
+        }
+        
+        cout << endl;
+    }
+
+// pattern 9
+/*
+    *    
+   ***   
+  *****  
+ ******* 
+*********
+*********
+ ******* 
+  *****  
+   ***   
+    *    
+*/
+for(int i = 0; i < n; i++){
+        // space 
+        for(int j = 0; j < n - i - 1; j++){
+          cout << " ";
+        }
+        
+        // star
+        for(int j = 0; j < 2 * i + 1; j++){
+            cout << "*";
+        }
+        
+        // space
+        for(int j = 0; j < n - i - 1; j++){
+          cout << " ";
+        }
+        
+        cout << endl;
+    }
+    
+    
+    for(int i = 0; i < n; i++){
+        // space 
+        for(int j = 0; j < i; j++){
+          cout << " ";
+        }
+        
+        // star
+        for(int j = 0; j < 2 * n - (2 * i + 1); j++){
+            cout << "*";
+        }
+        
+        // space
+        for(int j = 0; j < i; j++){
+          cout << " ";
+        }
+        
+        cout << endl;
+    }
+return 0;
+}
---
+++ Patterns/readme.md
@@ -0,0 +1,4 @@
+# Pattern printing for logic building
+
+Reference : 
+https://takeuforward.org/strivers-a2z-dsa-course/must-do-pattern-problems-before-starting-dsa/
